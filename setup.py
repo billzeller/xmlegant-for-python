8666ee9e6d16945069c3e042a96c645eb0096e13
Bill Zeller <billiam@gmail.com>
2009-03-30 20:58:11 -0400

    added setup.py (for pypi distribution) and .6 and .7 files

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+import sys
+if sys.hexversion < 0x02050200:
+   raise RuntimeError, "Python 2.5.2 or higher required"
+
+
+setup(name='XMLegant',
+      version='0.7',
+	  description='A class allowing easy XML generation',
+	  #summary='A class allowing easy XML generation. XML can be generated using chaining methods or a succinct accessor syntax.',
+	  author='Bill Zeller',
+	  author_email='billiam@gmail.com',
+	  url='http://xmlegant.from.bz',
+	  license='BSD',
+      py_modules=['XMLegant'],
+      classifiers   = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: XML',
+        ],
+      
+	  )
+
+
